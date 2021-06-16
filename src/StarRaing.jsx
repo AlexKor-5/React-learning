0a223e80a473bc5ebc24a0ca446d29a7b2ae2b1b
Alexander Korniichuk <world.kor.films@gmail.com>
2021-06-16 09:44:48 +0300

    cards of colors

--- src/StarRaing.jsx
+++ src/StarRaing.jsx
@@ -3,13 +3,14 @@ import {FaStar} from "react-icons/fa";
 
 const createArray = length => [...Array(length)];
 
-const StarRating = ({ totalStars = 5, selectedStars = 0 }) => {
+const StarRating = ({ totalStars = 5, selectedStars = 0 ,onRate = f => f}) => {
     return (
         <>
             {createArray(totalStars).map((n, i) => (
                 <Star
                     key={i}
                     selected={selectedStars > i}
+                    onSelect={() => onRate(i + 1)}
                 />
             ))}
             <p>
